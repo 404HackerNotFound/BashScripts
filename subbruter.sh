2b4c46676194509713cfbe989dced67ff77a7bfe
clirimemini <40791655+clirimemini@users.noreply.github.com>
2018-11-29 01:17:24 +0100

    Add files via upload

---
+++ subbruter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "======================================================"
+echo "================ Subdomain Bruteforcer ==============="
+echo "======================================================"
+if [[ $1 == '' ]] || [[ $2 == '' ]] || [[ $3 == '' ]] || [[ $1 == '-h' ]] || [[ $1 == '--help' ]]; then
+	echo "Usage - $0 target.com wordlist.txt output.txt"
+	exit
+fi
+
+for i in $(cat $2);
+do
+        cmd=$(curl --write-out %{http_code} --silent --output /dev/null -m 5 $i.$1)
+	if [ $cmd -eq 000 ]; then
+	  continue
+	else
+	  echo "$i.$1 = Found" && echo "$i.$1" >> $3
+	fi
+done
+echo "Completed."
+
